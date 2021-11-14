f9930323d6fb3282bc529c28e9d5bad448d5b83e
Chance <chancecrawford30@gmail.com>
2021-11-13 23:26:26 -0800

    player must enter valid name

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,11 +8,24 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+//we could have put this function inside of the playerInfo object
+// but we chose this way
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 
 // player info object houses the player variables.
 //see google docs javascript notes, objects
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
